3cf8b690b3474d143810e51ecc7eb965e6269889
Nick <dfer231314241@gmail.com>
2018-12-15 19:23:00 +0800

    UIconnection

---
+++ Assets/Scripts/click_gaming.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class click_gaming : MonoBehaviour {
+
+    public GameObject pauseMode;
+    bool state = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (state)
+            {
+                pauseMode.gameObject.SetActive(false);
+                state = false;
+            }
+            else
+            {
+                pauseMode.gameObject.SetActive(true);
+                state = true;
+            }
+        }
+    }
+
+    public void pause()
+    {
+        pauseMode.gameObject.SetActive(false);
+        state = false;
+    }
+
+    public void save()
+    {
+        Scene cur_scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(0);
+    }
+}
